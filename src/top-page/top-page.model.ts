e124c5edf73e5ce975d9f2821f8dde882824cf4c
Artem Veremienko <temaspsxxx@gmail.com>
2021-12-09 21:01:54 +0200

    feat: add top-page model props

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -5,23 +8,60 @@ export enum TopLevelCategory {
   Products,
 }
 
-export class TopPageModel {
-  _id: string;
-  firstCategory: TopLevelCategory;
-  secondCategory: string;
+export class HhData {
+  @Prop()
+  count: number;
+
+  @Prop()
+  juniorSalary: number;
+
+  @Prop()
+  middleSalary: number;
+
+  @Prop()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+  @Prop()
   title: string;
+
+  @Prop()
+  description: string;
+}
+
+@Schema()
+export class TopPageModel {
+  @Prop({ enum: TopLevelCategory })
+  firstCategory: TopLevelCategory;
+
+  @Prop()
+  secondCategory: string;
+
+  @Prop({ unique: true })
+  alias: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
   category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+
+  @Prop({ type: HhData, _id: false })
+  hh?: HhData;
+
+  @Prop({ type: () => [TopPageAdvantage], _id: false })
+  advantages: TopPageAdvantage[];
+
+  @Prop()
   seoText: string;
+
+  @Prop()
   tagsTitile: string;
+
+  @Prop([String])
   tags: string[];
 }
+
+export type TopPageModelDocument = TopPageModel & Document;
+export const TopPageModelSchema = SchemaFactory.createForClass(TopPageModel);
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,14 @@
+import { TopPageModel, TopPageModelSchema } from './top-page.model';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
 
 @Module({
-  controllers: [TopPageController]
+  imports: [
+    MongooseModule.forFeature([
+      { name: TopPageModel.name, schema: TopPageModelSchema },
+    ]),
+  ],
+  controllers: [TopPageController],
 })
 export class TopPageModule {}
