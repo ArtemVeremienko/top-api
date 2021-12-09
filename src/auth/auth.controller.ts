1eceabee84e802bd42b1c4e79c2120cb8eebde8d
Artem Veremienko <temaspsxxx@gmail.com>
2021-12-09 02:49:26 +0200

    feat: add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { AuthDto } from './dto/auth.dto';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  @Post('register')
+  async register(@Body() dto: AuthDto) {}
+
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto) {}
+}
