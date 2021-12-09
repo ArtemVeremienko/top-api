c0a3c9dd7df4b673753abaff967ac10d2567d280
Artem Veremienko <temaspsxxx@gmail.com>
2021-12-09 21:01:26 +0200

    feat: add auth model props

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,14 @@
-export class AuthModle {
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class AuthModel {
+  @Prop({ unique: true })
   email: string;
+
+  @Prop()
   passwordHash: string;
 }
+
+export type AuthModelDocument = AuthModel & Document;
+export const AuthModelSchema = SchemaFactory.createForClass(AuthModel);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,14 @@
+import { AuthModel, AuthModelSchema } from './auth.model';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController]
+  imports: [
+    MongooseModule.forFeature([
+      { name: AuthModel.name, schema: AuthModelSchema },
+    ]),
+  ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
