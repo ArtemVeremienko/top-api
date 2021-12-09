bb8ee420f05808ef7fb4bdd34f1490831d9cef9e
Artem Veremienko <temaspsxxx@gmail.com>
2021-12-09 21:01:44 +0200

    feat: add review model props

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,20 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
 export class ReviewModel {
-  _id: string;
+  @Prop()
   name: string;
+
+  @Prop()
   title: string;
+
+  @Prop()
   description: string;
+
+  @Prop()
   rating: number;
-  createdAt: Date;
 }
+
+export type ReviewModelDocument = ReviewModel & Document;
+export const ReviewModelSchema = SchemaFactory.createForClass(ReviewModel);
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,14 @@
+import { ReviewModel, ReviewModelSchema } from './review.model';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
 
 @Module({
-  controllers: [ReviewController]
+  imports: [
+    MongooseModule.forFeature([
+      { name: ReviewModel.name, schema: ReviewModelSchema },
+    ]),
+  ],
+  controllers: [ReviewController],
 })
 export class ReviewModule {}
