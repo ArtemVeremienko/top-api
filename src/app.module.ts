cc7c63bbadde026c59577ae5b001436a274b24f6
Artem Veremienko <temaspsxxx@gmail.com>
2021-12-10 20:57:52 +0200

    del: remove unused components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
@@ -22,7 +20,5 @@ import { MongooseModule } from '@nestjs/mongoose';
     ProductModule,
     ReviewModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
